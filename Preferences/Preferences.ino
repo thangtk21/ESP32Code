26d99b63fe60b1c6df8a9456210b18f66f7b1d13
thangtk21 <thangtk21ntt@gmail.com>
2020-11-25 13:45:13 +0700

    preferences

---
+++ Preferences/Preferences.ino
@@ -0,0 +1,16 @@
+#include "Preferences.h"
+Preferences pr;
+
+void setup() 
+{
+  Serial.begin(115200);
+  pr.begin("DEVN",false);
+  uint8_t a = pr.getUInt("cnt",0);
+  Serial.println(a);
+  pr.end();
+}
+
+void loop() 
+{
+
+}
